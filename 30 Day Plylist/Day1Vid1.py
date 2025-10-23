7721255395749a420c2e7124b0f03f5e9e6438a0
Nama Kamu <emailmu@example.com>
2025-10-23 18:08:19 +0700

    Another Perubahan

--- 30 Day Plylist/Day1Vid1.py
+++ 30 Day Plylist/Day1Vid1.py
@@ -18,14 +18,17 @@ dijalankan dari direktori yang berbeda.
 '''
 import cv2
 import numpy
+import os
 
 image = cv2.imread(r'D:\Azqya Old Code 2\PY and NumPy\30 Day Plylist\azqya.jpg')
+# image = cv2.imread(r'D:\Azqya Old Code 2\PY and NumPy\30 Day Plylist\burung.jpg')
+# image = os.path.join('D:', 'Azqya Old Code 2', 'PY and NumPy', '30 Day Plylist', 'azqya.jpg')
 # print(image) 
 # # print(image.shape)
 
-window_name = 'Gambarku'
-cv2.namedWindow(window_name,cv2.WINDOW_NORMAL) #memungkinkan ubah ukuran jendela
-cv2.resizeWindow(window_name,400,420)
+# window_name = 'Gambarku'
+# cv2.namedWindow(window_name,cv2.WINDOW_NORMAL) #memungkinkan ubah ukuran jendela
+# cv2.resizeWindow(window_name,400,420)
 
 '''
 if image is not None :
@@ -67,8 +70,18 @@ Tujuan dari ROI adalah mengambil sub-array (bagian kecil) dari gambar asli.
 
 '''
 
-roi = image[700:1100, 700:900]
+# roi = image[700:1100, 700:900]
 
-cv2.imshow(window_name, roi)
+# cv2.imshow(window_name, roi)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+##########################   RESIZE  && COLOR  ##########################
+resize_image = cv2.resize(image,(640,480))
+
+img_color = cv2.cvtColor(resize_image, cv2.COLOR_BGR2RGB)
+
+cv2.imshow('gambar', img_color)
+
+# cv2.imshow('gambar', resize_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
