655e0cdfd63ad8fc34c29d46cebb3d2d0e6d6ddf
Nama Kamu <emailmu@example.com>
2025-10-23 15:27:25 +0700

    Perubahan yang baik

---
+++ 30 Day Plylist/Day1Vid1.py
@@ -0,0 +1,58 @@
+'''
+1. numpy
+2. Fungsi os.path,Tujuan & Kegunaan
+
+os.path.join(),
+Menggabungkan komponen jalur secara cerdas. 
+Ini adalah fungsi terpenting. Daripada menulis 'folder' + '/' + 'file.jpg', 
+Anda bisa menulis os.path.join('folder', 'file.jpg'). Fungsi ini akan otomatis 
+menggunakan pemisah jalur yang benar (/ di Linux/macOS, \ di Windows).
+
+os.path.exists(),
+Memeriksa apakah file/folder ada. Digunakan untuk pengecekan error yang baik 
+sebelum memanggil cv2.imread(). Ini mencegah AttributeError: 'NoneType' jika gambar tidak ditemukan.
+
+os.path.abspath(),
+Mengubah jalur relatif menjadi jalur absolut (lengkap). Membantu menghilangkan ambiguitas saat program
+dijalankan dari direktori yang berbeda.
+'''
+import cv2
+import numpy
+
+image = cv2.imread(r'D:\Azqya Old Code 2\PY and NumPy\30 Day Plylist\azqya.jpg')
+# print(image) 
+# # print(image.shape)
+
+window_name = 'Gambarku'
+cv2.namedWindow(window_name,cv2.WINDOW_NORMAL) #memungkinkan ubah ukuran jendela
+cv2.resizeWindow(window_name,400,420)
+
+'''
+if image is not None :
+    window_name = 'Gambarku'
+    cv2.namedWindow(window_name,cv2.WINDOW_NORMAL) #memungkinkan ubah ukuran jendela
+    cv2.resizeWindow(window_name,800,600)
+    cv2.imshow(window_name, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else :
+    print ("ERORRRRRRRR HUHUUU TT")
+print(image)
+'''
+# MENGUBAH DAN MENGAKSES PIKSEL TERTENTU
+'''
+cara mengakses nilai BGR dari piksel tunggal, misalnya 
+piksel di koordinat (100, 50). Ini adalah kunci untuk 
+memahami bagaimana cara mengubah warna pada titik tertentu
+di gambar.
+'''
+pixel_value = image[100,50]
+
+print(f"Nilai BGR piksel(100,50) : {pixel_value}")
+
+image[ 700 :900, 200:400] = [0, 200, 100]
+image[700:900, 1000:1200] = [100, 0, 1]
+image[100:300, 1000:1200] = [100, 0, 1]
+cv2.imshow(window_name,image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
